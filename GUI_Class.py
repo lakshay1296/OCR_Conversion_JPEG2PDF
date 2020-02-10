22b8694d619ab819f289d2732e5cf5a5897aac8c
lakshay1296 <lakshay1296@gmail.com>
2020-02-10 19:27:38 +0530

    Added text conversion, though, it does not work independently

--- GUI_Class.py
+++ GUI_Class.py
@@ -26,7 +26,7 @@ class window(object):
 
         self.menu_bar(self.master)
 
-        Label(self.master, text="Tesseract Powered OCR", bg="white", height=2, width=55) \
+        Label(self.master, text="Tesseract Powered OCR", bg="white", height=2, width=60) \
             .grid(row=0, sticky=NW, padx=10, pady=5)
 
         select_pdf = BooleanVar()
@@ -37,8 +37,13 @@ class window(object):
         hocr = Checkbutton(self.master, text=" hOCR", variable=select_hocr) \
             .grid(sticky=NE, padx=15, row=1, column=0)
 
+        select_text = BooleanVar()
+        txt = Checkbutton(self.master, text=" TXT", variable=select_text) \
+            .grid(sticky=N, padx=30, row=1, column=0)
+
         var_list.append(select_pdf)
         var_list.append(select_hocr)
+        var_list.append(select_text)
 
         combo = ttk.Combobox(self.master)
         combo["values"] = ("Select PSM mode", 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13)
@@ -47,20 +52,16 @@ class window(object):
         var_list.append(combo)
 
         # Button for opening folder location
-        folder_open = Button(self.master, text="Select Folder Location", command=lambda: self.folderopen()) \
+        folder_open = Button(self.master, text="Select Folder Location", command=lambda: self.folderopen(), width=19) \
             .grid(sticky=NW, padx=15, pady=5, row=3)
 
         # Button for opening files
-        file_open = Button(self.master, text="Select PDF Location", command=lambda: self.fileopen()) \
+        file_open = Button(self.master, text="Select PDF Location", command=lambda: self.fileopen(), width=19) \
             .grid(sticky=NW, padx=15, pady=5, row=4)
 
-        # Button for submitting options
-        # submit_button = Button(self.master, text="Submit", command=lambda: self.submit(var_list)) \
-        #     .grid(sticky=NW, padx=15, pady=5)
-
         # Button which activates the progress bar
-        add_bar = Button(self.master, text="Real Submit", command=lambda: self.start_thread(var_list), bd=1, relief=SOLID) \
-            .grid(sticky=NW, padx=15, pady=5, row=5)
+        add_bar = Button(self.master, text="Submit", command=lambda: self.start_thread(var_list), bd=1, relief=SOLID, width=19) \
+            .grid(sticky=SW, padx=15, pady=5, row=5)
 
     def submit(self, var_list):
         # list is [pdf, ocr, psm mode]
@@ -121,7 +122,80 @@ class window(object):
         menubar.add_cascade(label="Menu", menu=fileMenu)
         fileMenu.add_command(label="Exit", command=master.quit)
         menubar.add_cascade(label="About", menu=helpMenu)
-        helpMenu.add_command(label="Help")
+        helpMenu.add_command(label="Help", command=self.tesseract_help)
+
+    def help_text(self, text):
+
+        text.insert(END, "\nTesseract --help:\n", "big")
+        # Config Text
+        text.insert(END, "\n-c CONFIGVAR=VALUE\n", "bold_italics")
+        text.insert(END, "\tSet value for parameter CONFIGVAR to VALUE. Multiple -c arguments are allowed.\n", "large")
+        # DPI Text
+        text.insert(END, "\n--dpi N\n", "bold_italics")
+        text.insert(END, "\tSpecify the resolution N in DPI for the input image(s). A typical value for N is 300. \n\t"
+                         "Without this option, the resolution is read from the metadata included in the image. \n\t"
+                         "If an image does not include that information, Tesseract tries to guess it.\n", "large")
+        # Lang Text
+        text.insert(END, "\n-l LANG\n", "bold_italics")
+        text.insert(END, "\tSpecify the language L for the detection language. Additional language packs are\n\t"
+                         "required for different languages\n", "large")
+        # Script Text
+        text.insert(END, "\n-l SCRIPT\n", "bold_italics")
+        text.insert(END, "\tThe language or script to use. If none is specified, eng (English) is assumed. Multiple "
+                         "\n\tlanguages may be specified, separated by plus characters. Tesseract uses\n\t"
+                         "3-characterISO 639-2 language codes (see LANGUAGES AND SCRIPTS).\n", "large")
+        # PSM Text
+        text.insert(END, "\n--psm N\n", "bold_italics")
+        text.insert(END, "\tSet Tesseract to only run a subset of layout analysis and assume a certain form of"
+                         "\n\timage. The options for N are:\n","large")
+        psm_mode = """
+        \t0 = Orientation and script detection (OSD) only.
+        \t1 = Automatic page segmentation with OSD.
+        \t2 = Automatic page segmentation, but no OSD, or OCR. (not implemented)
+        \t3 = Fully automatic page segmentation, but no OSD. (Default)
+        \t4 = Assume a single column of text of variable sizes.
+        \t5 = Assume a single uniform block of vertically aligned text.
+        \t6 = Assume a single uniform block of text.
+        \t7 = Treat the image as a single text line.
+        \t8 = Treat the image as a single word.
+        \t9 = Treat the image as a single word in a circle.
+        \t10 = Treat the image as a single character.
+        \t11 = Sparse text. Find as much text as possible in no particular order.
+        \t12 = Sparse text with OSD.
+        \t13 = Raw line. Treat the image as a single text line, bypassing hacks \n\tthat are Tesseract-specific.
+        """
+        text.insert(END, psm_mode, "italics")
+        # OEM Text
+        text.insert(END, "\n--oem N\n", "bold_italics")
+        text.insert(END, "\tSpecify OCR Engine mode. The options for N are:\n","large")
+        oem_mode = """
+        \t0 = Original Tesseract only.
+        \t1 = Neural nets LSTM only.
+        \t2 = Tesseract + LSTM.
+        \t3 = Default, based on what is available.
+        """
+        text.insert(END, oem_mode, "italics")
+        # OEM Text
+        text.insert(END, "\n--tessdata-dir PATH\n", "bold_italics")
+        text.insert(END, "\tSpecify the location of tessdata path.\n","large")
+        # OEM Text
+        text.insert(END, "\n--user-patterns FILE\n", "bold_italics")
+        text.insert(END, "\tSpecify the location of user patterns file.\n","large")
+        # OEM Text
+        text.insert(END, "\n--user-words FILE\n", "bold_italics")
+        text.insert(END, "\tSpecify the location of user words file.\n","large")
+
+    def tesseract_help(self):
+        window = Tk()
+        window.title("Tesseract Help")
+        Scrollbar(window).pack(side=RIGHT, fill=Y)
+        text = Text(window)
+        text.tag_configure('bold_italics', font=('Arial', 11, 'bold', 'italic'))
+        text.tag_configure('big', font=('Verdana', 18, 'bold'))
+        text.tag_configure('large', font=('Verdana', 10))
+        text.tag_configure('italics', font=('Verdana', 10, 'italic'))
+        self.help_text(text)
+        text.pack(side=LEFT, fill=Y)
 
     def add_bar(self):
         var = IntVar()
@@ -141,7 +215,7 @@ class window(object):
         # t = Thread(target=lambda: self.add_bar())
         self.commands = []
 
-        psm_mode = " --psm " + str(var_list[2].get())
+        psm_mode = " --psm " + str(var_list[3].get())
         self.commands.append(psm_mode)
 
         # PDF Conversion
@@ -153,6 +227,13 @@ class window(object):
         if var_list[1].get() is True:
             print("HOCR has been selected.")
             self.commands.append(" hocr")
+
+        # HOCR Conversion
+        if var_list[2].get() is True:
+            print("TXT has been selected.")
+            self.commands.append(" txt")
+
+        print (self.commands)
 
         if self.function == "Initiated fileopen":
             t = Thread(target=lambda: self.ocr_code_singleFile(pdf_location=self.filename, var_list=var_list, command=self.commands))
@@ -173,7 +254,7 @@ class window(object):
         var.set(0)
 
         Label(self.master, text="OCR Progess: ", font="Helvetica 9 bold").grid(sticky=NW, padx=15, pady=5, row=6)
-        progessbar = ttk.Progressbar(self.master, variable=var, orient=HORIZONTAL, length=200).grid(sticky=NW, padx=15, pady=5, row=7)
+        progessbar = ttk.Progressbar(self.master, variable=var, orient=HORIZONTAL, length=200).grid(sticky=EW, padx=15, pady=5, row=7)
 
         path = pdf_location
 
@@ -186,6 +267,12 @@ class window(object):
         pdf_file = path_list[len(path_list) - 1]
 
         ocr_path = path.replace(".pdf", "_ocr.pdf")
+        ocr_txt_path = path.replace(".pdf", "_ocr.txt")
+
+        if (len(self.commands) > 2) and self.commands[2] == " txt":
+            txt = open(ocr_txt_path, "wb")
+        else:
+            txt = None
 
         pdf_writer = PdfFileWriter()
 
@@ -206,6 +293,7 @@ class window(object):
             ''' pdf merge '''
 
             pdf_path = jpeg_path.replace(".jpeg", ".pdf")
+
             pdf_obj = open(pdf_path, "rb")
             pdf_reader = PdfFileReader(pdf_obj)
 
@@ -219,15 +307,29 @@ class window(object):
 
             pdf_obj.close()
 
+            """ Text File Generation """
+            if len(self.commands) > 2 and self.commands[2] == " txt":
+                txt_path = jpeg_path.replace(".jpeg", ".txt")
+                txt_obj = open(txt_path, "rb")
+
+                content = txt_obj.read()
+                if txt is not None:
+                    txt.write(content)
+
+                txt_obj.close()
+                os.remove(txt_path)
+
             os.remove(pdf_path)
 
             # For calculating the OCR Progress
             x = (count / page_count) * 100
             percentage = round(x)
             var.set(percentage)
-            time.sleep(5.0)
+            # time.sleep(5.0)
             count = count + 1
 
+        if txt is not None:
+            txt.close()
         print("OCR'd " + pdf_file + " successfully.")
         messagebox.showinfo(title="OCR Complete", message=pdf_file + " has been OCR'd")
 
@@ -245,10 +347,10 @@ class window(object):
         labelVar.set("Files Progress: ")
 
         Label(self.master, text="OCR Progess: ", font="Helvetica 9 bold").grid(sticky=NW, padx=15, pady=5, row=6)
-        progessbar = ttk.Progressbar(self.master, variable=var, orient=HORIZONTAL, length=200).grid(sticky=NW, padx=15, pady=5, row=7)
+        progessbar = ttk.Progressbar(self.master, variable=var, orient=HORIZONTAL, length=200).grid(sticky=EW, padx=15, pady=5, row=7)
         label1 = Label(self.master, text=labelVar.get(), font="Helvetica 9 bold")\
             # .grid(sticky=NW, padx=15, pady=5)
-        progessbar = ttk.Progressbar(self.master, variable=var1, orient=HORIZONTAL, length=200).grid(sticky=NW, padx=15, pady=5, row=9)
+        progessbar = ttk.Progressbar(self.master, variable=var1, orient=HORIZONTAL, length=200).grid(sticky=EW, padx=15, pady=5, row=9)
         print (label1["text"])
 
         file_count_list = []
